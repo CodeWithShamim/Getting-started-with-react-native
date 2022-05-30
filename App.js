5f81aa6c78ecd237f23180a733919b7ca6887972
codewithshamim <shamimislamonline@gmail.com>
2022-05-30 21:34:43 +0600

    style added

--- App.js
+++ App.js
@@ -7,42 +7,49 @@ import {
   Button,
   Alert,
   ScrollView,
+  ActivityIndicator,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import Countries from "./components/Countries";
 
 export default function App() {
   const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
-      .then((data) => setCountries(data));
+      .then((data) => {
+        setLoading(false);
+        setCountries(data);
+      });
   }, []);
-
-  console.log(countries);
 
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text style={styles.heading}>Welcome to CF Country Hunter</Text>
       <StatusBar style="auto" />
       <Image
         source={{
           uri: "https://yt3.ggpht.com/ytc/AKedOLQv2rLgjj2loLBpLSn5DGdGt2LLd75AwSJuZ040Lw=s900-c-k-c0x00ffffff-no-rj",
         }}
-        style={{ width: 200, height: 200 }}
+        style={{ width: 200, height: 200, borderRadius: 100, marginBottom: 10 }}
       />
+
       <Button
         onPress={() => Alert.alert("Hello")}
-        title="Learn More"
+        title="Visit Our Website"
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
       />
 
       {/* _____________ */}
-      <ScrollView>
+      <ScrollView style={{ marginTop: 50 }}>
         {countries?.map((country) => (
           <Countries key={country.id} country={country}></Countries>
         ))}
+        {loading && <ActivityIndicator size="large" color="#00ff00" />}
       </ScrollView>
     </View>
   );
@@ -54,5 +61,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  heading: {
+    marginBottom: 10,
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 30,
+    padding: 20,
+    backgroundColor: "black",
   },
 });
--- components/Countries.js
+++ components/Countries.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
 
 const Countries = ({ country }) => {
   return (
-    <View>
+    <View style={styles.cardContainer}>
       <Text>{country.name.common}</Text>
+      <Image
+        source={{ uri: country.flags.png }}
+        style={{
+          width: 100,
+          height: 100,
+          borderRadius: 100,
+          display: "flex",
+        }}
+      />
     </View>
   );
 };
 
 export default Countries;
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    marginBottom: 15,
+  },
+});
